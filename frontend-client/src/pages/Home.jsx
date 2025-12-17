a64417a94b9d2faac664406ad81fc01ed9af2d24
Ismat <houbadismatmohamed@icloud.com>
2025-12-17 23:01:04 +0100

    Update Home.jsx

--- frontend-client/src/pages/Home.jsx
+++ frontend-client/src/pages/Home.jsx
@@ -1,9 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>Plateforme de livraison locale</h1>
       <p>Autorisez la localisation pour continuer.</p>
-      <button>Autoriser la localisation</button>
+
+      <button onClick={() => navigate("/restaurants")}>
+        Autoriser la localisation
+      </button>
     </div>
   );
 }
