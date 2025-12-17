28384bb96ed5b828374cc2d3de203f6c03ca7e1c
Ismat <houbadismatmohamed@icloud.com>
2025-12-17 23:00:28 +0100

    Update App.jsx

--- frontend-client/src/App.jsx
+++ frontend-client/src/App.jsx
@@ -1,3 +1,21 @@
-import Home from './pages/Home';
-function App(){ return <Home /> }
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Restaurants from "./pages/Restaurants";
+import Menu from "./pages/Menu";
+import Cart from "./pages/Cart";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/restaurants" element={<Restaurants />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 export default App;
